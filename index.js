33d660c9d9bb6b492352658430ed42831b7ac6de
chang <cshin7374@gmail.com>
2022-04-07 23:39:49 -0500

    commit tree 25933ca5cdb3ba5aa5d28239a9929da895738ca3

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    return console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    return console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    }
+    else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    else if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
+
+// function sayHiToHeadphonedRoommate(string) {
+//     const lowStr = string.toLowerCase()
+//     const upStr = string.toUpperCase()
+//     const str = "let's have dinner together!"
+
+//     switch (string) {
+//         case lowStr: "I can't hear you!";
+//             break;
+//         case upStr: "YES INDEED!";
+//             break;
+//         case str: "I would love to!";
+//             break;
+//     }
+
+// }
